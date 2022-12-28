31f9f2ecae2793f883400d8016f529ed61be79bb
Susmitpr <113661126+Susmitpr@users.noreply.github.com>
2022-12-29 00:11:43 +0530

    Delete 4_nor_verilog/INCA_libs directory

--- 4_nor_verilog/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,5 +0,0 @@
-1652175306 /home/MSIS/cds.lib
-1666084401 /home/MSIS/Documents/susmit/3_or_verilog/or.sv
-1666084427 /home/MSIS/Documents/susmit/3_or_verilog/or_tb.sv
-1666087568 /home/MSIS/Documents/susmit/4_nor_verilog/nor.sv
-1666087589 /home/MSIS/Documents/susmit/4_nor_verilog/nor_tb.sv
