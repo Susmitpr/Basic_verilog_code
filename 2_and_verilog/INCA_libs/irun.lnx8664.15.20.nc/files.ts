cedf9c149bc1482d80fb0ca80f72b0a1b68b1523
Susmitpr <113661126+Susmitpr@users.noreply.github.com>
2022-12-29 00:11:34 +0530

    Delete 2_and_verilog directory

--- 2_and_verilog/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,3 +0,0 @@
-1652175306 /home/MSIS/cds.lib
-1666084297 /home/MSIS/Documents/susmit/susmit verilog/2_and_verilog/and.sv
-1666084313 /home/MSIS/Documents/susmit/susmit verilog/2_and_verilog/and_tb.sv
